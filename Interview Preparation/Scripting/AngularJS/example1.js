b249fb7ef526b2a9df77539ce379a1dcd8a0c240
Awad Bin-Jawed <awadbinjawed@gmail.com>
2019-06-26 10:30:53 -0700

    Update example1.js

--- Interview Preparation/Scripting/AngularJS/example1.js
+++ Interview Preparation/Scripting/AngularJS/example1.js
@@ -1,3 +1,4 @@
+//Basic AngularJS Example with directives :
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,5 +12,22 @@
     <p>Name: <input type="text" ng-model="name"></p>
     <p ng-bind="name"></p>
   </div>
+  </body>
+</html>
+
+
+//Basic AngularJS Example with directives, plus ng-init :
+<!DOCTYPE html>
+<html>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+  <body>
+  <div ng-app="" ng-init="firstName='Awad'">
+    <p>Input something in the input box:</p>
+    <p>Name: <input type="text" ng-model="firstName"></p>
+    <p>You wrote: {{ firstName }}</p>
+  </div>
+
   </body>
 </html>
