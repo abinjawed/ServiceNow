eec95c4d2be6a644fd1b49124d08845e3e62b42d
Awad Bin-Jawed <awadbinjawed@gmail.com>
2020-05-19 11:40:28 -0600

    Create sortByNameOrAge.js

---
+++ Week 1/Coding Algorithms and Data Structures/Coding Algorithms/Differentiate Between a Stable and Unstable Sorting Algorithm?/sortByNameOrAge.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+var people = [
+{name: 'Ahmad', age: 64},
+{name: 'Nadia', age: 51},
+{name: 'Awad', age: 32},
+{name: 'Omer', age: 31},
+{name: 'Abeer', age: 29},
+];
+
+//Sort by name
+people.sort( (p1, p2) => {
+if (p1.name < p2.name) return -1;
+if (p1.name > p2.name) return 1;
+return 0;
+});
+
+console.log(people.map(p => p.name));
+
+//Sort by age
+people.sort((p1, p2) =>{
+  if (p1.age < p2.age) return -1;
+  if (p1.age > p2.age) return 1;
+  return 0;
+});
+
+console.log(people.map(p => p.name));
+</script>
+</body>
+</html>
